8182ca2e6f2a4f9df1f5fef6d1d16fb0ecf2150a
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 19:44:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 class Song
-  extend Memorable
-  extend Findable
+  extend Memorable::ClassMethods
+  include Findable::InstanceMethods
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :artist
